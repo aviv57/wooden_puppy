d89245b7e697cff008b94170ed108d4dbe279590
aviv57 <aviv57@gmail.com>
2023-09-08 21:43:49 +0300

    Adding title, favicon, and updating css

--- frontend/wooden_puppy_frontend/src/index.tsx
+++ frontend/wooden_puppy_frontend/src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+document.title = "Wooden Puppy"
 root.render(
   <React.StrictMode>
     <App />
